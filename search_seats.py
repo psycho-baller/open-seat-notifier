ec1c395cfe3b69ab100c23a00ad568cc7a85622c
psycho-baller <Ramim66809@gmail.com>
2022-08-01 01:14:04 +0400

    decided to just use "local" server

--- search_seats.py
+++ search_seats.py
@@ -10,16 +10,17 @@ import warnings
 from bs4 import BeautifulSoup as bs
 # import webbrowser
 
-
-
 Options = Options()
 Options.add_argument("--headless")
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
-    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),options=Options)
+    driver = webdriver.Firefox(
+        executable_path=GeckoDriverManager().install(), options=Options)
 # remove the options argument if u wanna see the browser open and perform the automated process
 # %%
-def get_links(username,password):
+
+
+def get_links(username, password):
     url = 'https://ucalgary.sona-systems.com'
     user_ID = username
     password = password
@@ -41,6 +42,8 @@ def get_links(username,password):
     soup = bs(html, 'html.parser')
     table_row = soup.find('tr').parent.findNextSibling()
     study_links = table_row.findAll('a')
+
+    driver.quit()
 
     links = set()
     for link in study_links:
