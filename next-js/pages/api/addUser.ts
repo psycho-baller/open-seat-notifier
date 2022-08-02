0f7e282a01fdead5fed9a2bc95a42cd5d86eaf3e
psycho-baller <Ramim66809@gmail.com>
2022-08-02 04:01:56 +0400

    attempt 16

--- next-js/pages/api/addUser.ts
+++ next-js/pages/api/addUser.ts
@@ -16,6 +16,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     email: email,
     username: username,
     password: password,
+    id: 1,
   };
   try {
     const addUser = await prisma.main.create({ data: user });
